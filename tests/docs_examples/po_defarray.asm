fe9136b156542cb44624a5862edd4a188789e0e0
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-03-11 10:40:06 +0100

    tests: Issue #33 test adjusted (fix will be in following commits)

--- tests/docs_examples/po_defarray.asm
+++ tests/docs_examples/po_defarray.asm
@@ -1,9 +1,6 @@
-;;; this example is currently malfunctioning ... to be fixed one day...
-;;; https://github.com/z00m128/sjasmplus/issues/33
-    DISPLAY "Currently malfunctioning (Issue #33), the BIN is intentionally disabled to 'pass'"
-
 ;;; the example is slightly modified to produce some actual binary data for automatic testing
-    DEFARRAY myarray 10*20,"A",20,40,50,70
+    DEFARRAY myarray 5*13,"B",67,40+28,'e'-32,'F'
+    ; And the produced data should be ASCII string "ABCDEF"
 
     OUTPUT 'po_defarray.bin'
 
