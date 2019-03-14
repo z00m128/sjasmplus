77d24fc92177d8593025f77cf3ea24d342eda330
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-03-14 19:19:50 +0100

    .aftermath of fix of END directive = removing old text from test

--- tests/misc/dir_end.asm
+++ tests/misc/dir_end.asm
@@ -9,9 +9,3 @@ end     DUP 2   ; END should not work at first column, not even with --dirbol en
         END : no start address provided, and this text should be NOT parsed either
 
 Some random text, which is not supposed to be assembled.
-
-For some reason the colon after END will cause the listing file to have one extra
-\t character on the very first source line, but I was unable to debug it and find
-reasonable fix. The tabulator itself is highly likely produced by
-sjio.cpp:ReadBufLine function, but it's not clear how it will end as first char
-of Listing output (read from `line` buffer).
