0682fbd2fead3a56740e88d40154b3c192609fd1
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-03-20 14:48:52 +0100

    tests: make the OS time in LUA truly fixed, to make CI testing work

--- tests/docs_examples/po_display.asm
+++ tests/docs_examples/po_display.asm
@@ -22,7 +22,10 @@ will output to the console strings:
 ;; Part of example in LUA chapter of documentation (modified to pin date to fixed one)
     LUA
         -- Creates define "TIME" with current time
-        sj.insert_define("TIME", '"' .. os.date("%Y-%m-%d %H:%M:%S", 906000490) .. '"')
+        datetime = os.date("%Y-%m-%d %H:%M:%S")
+        print(datetime)
+        datetime = "1982-04-23 03:14:15"    -- set it to fixed string for CI tests to pass
+        sj.insert_define("TIME", '"' .. datetime .. '"')
     ENDLUA
 
     DISPLAY "Build time: ", TIME
