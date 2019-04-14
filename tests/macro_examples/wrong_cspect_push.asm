0983eb99ee392cb0b08f344830f7da36d29b4779
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-04-14 21:14:19 +0200

    tests/macro-examples: MACRO to emit wrong `push imm16` for CSpect emulator

---
+++ tests/macro_examples/wrong_cspect_push.asm
@@ -0,0 +1,14 @@
+; macro to emit Z80N `push imm16` opcode either in big endian (HW board), or little
+; endian (CSpect emulator) way.
+
+    MACRO PUSH_IMM16 _value?
+        DB  $ED, $8A
+        IFDEF CSPECT : DB low _value?, high _value? : ELSE : DB high _value?, low _value? : ENDIF
+    ENDM
+
+    OUTPUT "wrong_cspect_push.bin"
+    push        0x1234
+    PUSH_IMM16  0x1234      ; should emit identical opcode as the sjasmplus itself
+
+    DEFINE CSPECT   ; uncomment, or provide option `-DCSPECT` on CLI to get wrong opcode
+    PUSH_IMM16  0x1234      ; should emit opcode with reversed HI/LO bytes
