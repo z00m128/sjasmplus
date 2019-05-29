4ccf543e1a30209aefcdd01cddc4a222f17b147a
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-05-29 23:54:29 +0200

    tests: for CSPECTMAP directive, also slightly relevant to Issue #48
    About #48: the new CSpect MAP file ("cspectmap.sym" file in this test)
    contains so far most internal information about labels (more than any
    other export available in sjasmplus before), BUT it's not precisely what
    was requested and the label name is modified:
    
    * "type" column can be used as indicator of "EQU/DEFL" labels: 00 =
    ordinary label, 01 = EQU/struct, 02 = DEFL (dynamic value)
    
    * labels are modified with "@" trying to split local part from primary
    label (so they differ from original label in listing file) - needs some
    fuzzy compare to find closest match
    
    * the file can't be easily modified/extended, because it is supposed to
    be parsed by CSpect, so the output format is defined by Mike Dailly.

---
+++ tests/devices/cspectmap.asm
@@ -0,0 +1,50 @@
+            DEVICE NONE         ; set "none" explicitly, to avoid "global device" feature
+            CSPECTMAP           ; error about non-device mode
+            DEVICE ZXSPECTRUMNEXT : SLOT 6
+            CSPECTMAP "cspectmap.sym"   ; default-name variant is tested only manually
+;             CSPECTMAP ; default-name is source name with ".map" appended (tested manually)
+            LABELSLIST "cspectmap.lbl"
+
+            ; create test labels in various areas of memory (and by various means)
+
+EquLabel    EQU     $1234
+
+DeflLabel   DEFL    $12+1
+
+            STRUCT Struct
+Field1      BLOCK   5
+Field2      DW      $5678
+            ENDS
+
+            ORG $C000, $00
+Page00:     daa
+.local:
+            ORG $C001, $01
+Page01:     scf
+.local:
+            ORG $C00A, $0A
+Page0A:     ccf
+.local:     rlca
+.codeEqu    EQU $+1             ; is like simple EQU => "wrong" physical address
+
+.local.local:       ; TODO this will be incorrectly marked as local at second dot
+    ; but it will stay so, because A) not simple to fix B) works often better for UX
+
+            ORG $DFFE, $0B      ; also raise difficulty by adding extra dots in labels
+Page0B.A:   cpl
+.local.b:
+            ORG $C0DF, $DF
+PageDF.A.B: rra
+.local.c..d:
+
+            ORG $C030, $30
+    MODULE Module
+Page30:     rla
+.local:
+    ENDMODULE
+
+            ORG $C031, $31
+    MODULE Module..two.
+Page31..A.: rrca
+.local..c.:
+    ENDMODULE
