05ded27708d7f1603ad71d5d3d0e0738376db27b
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-08-15 01:58:01 +0200

    CirrusCI: windows experiment, part 8

--- ContinuousIntegration/winbuild.bat
+++ ContinuousIntegration/winbuild.bat
@@ -1,22 +1,26 @@
 rem Exploring the VM image and what is available
 path
-dir /w
-dir /w C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin
-dir /w C:\tools\msys64
-dir /w C:\tools\msys64\clang32
-dir /w C:\tools\msys64\clang64
-dir /w C:\tools\msys64\mingw32
-dir /w C:\tools\msys64\mingw64
-dir /w C:\tools\msys64\opt
-dir /w C:\tools\msys64\usr
-dir /w C:\tools\msys64\usr\bin
-type C:\tools\msys64\msys2_shell.cmd
-
+@rem dir /w
+@rem dir /w C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin
+@rem dir /w C:\tools\msys64
+@rem dir /w C:\tools\msys64\clang32
+@rem dir /w C:\tools\msys64\mingw32
+@rem dir /w C:\tools\msys64\opt
+@rem dir /w C:\tools\msys64\usr
+@rem dir /w C:\tools\msys64\usr\bin
+@rem type C:\tools\msys64\msys2_shell.cmd
+dir /w C:\tools\msys64\clang64\bin
+dir /w C:\tools\msys64\mingw64\bin
+mingw64.exe gcc --version
+msys2_shell bash --version
+msys2 bash --version
+usr\bin\bash --version
+C:\tools\msys64\usr\bin\bash --version
 rem MinGW make experiments
-mingw32-make -f Makefile.win clean
+mingw32-make -f Makefile.win UNINSTALL='C:\tools\msys64\usr\bin\rm -f' clean
 mingw32-make -f Makefile.win
-mingw32-make -f Makefile.win PREFIX=c:/tools/sjasmplus install
-mingw32-make -f Makefile.win clean
+mingw32-make -f Makefile.win PREFIX=c:/tools/sjasmplus CREATEDIR='C:\tools\msys64\usr\bin\mkdir -p' install
+mingw32-make -f Makefile.win UNINSTALL='C:\tools\msys64\usr\bin\rm -f' clean
 dir /w c:\tools
 dir /w c:\tools\sjasmplus
 rem  bash ContinuousIntegration/test_folder_examples.sh
@@ -30,4 +34,5 @@ cd build
 cmake --help
 cmake -DCMAKE_BUILD_TYPE=Release ..
 dir /w
+vcvars64.bat
 msbuild sjasmplus.vcxproj
