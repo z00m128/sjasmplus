79c7d2ca1c17429ed9d097cba9a13286472a041d
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-08-15 00:34:12 +0200

    CirrusCI: windows experiment, part 4

--- ContinuousIntegration/winbuild.bat
+++ ContinuousIntegration/winbuild.bat
@@ -1,16 +1,19 @@
 path
 dir /w
-make -f Makefile.win clean
-make -f Makefile.win
-make -f Makefile.win install
-bash ContinuousIntegration/test_folder_examples.sh
-bash ContinuousIntegration\test_folder_examples.sh
-@rem  del Makefile
-@rem  ren Makefile.win Makefile
-@rem  dir Makefile
-@rem  mkdir build
-@rem  cd build
-@rem  cmake /?
-@rem  cmake --help
-@rem  cmake -DCMAKE_BUILD_TYPE=Release ..
-@rem  dir /w
+dir /w C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin
+dir /w C:\tools\msys64
+@rem MinGW make experiments
+mingw32-make --help
+rem  make -f Makefile.win clean
+rem  make -f Makefile.win
+rem  make -f Makefile.win install
+rem  bash ContinuousIntegration/test_folder_examples.sh
+rem  bash ContinuousIntegration\test_folder_examples.sh
+@rem CMAKE experiments
+del Makefile
+ren Makefile.win Makefile
+mkdir build
+cd build
+cmake --help
+cmake -DCMAKE_BUILD_TYPE=Release ..
+dir /w
