fca70cf9e30550faa9c8b5b52246276f02d605ed
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-08-15 06:50:04 +0200

    CirrusCI: configuring windows builds (MinGW + VS 15 2017)
    The tests are run only in the MinGW build, and their result is ignored
    at this moment, as some tests fail due to slash vs backslash in filename
    paths, and SHELLEXEC test suggests that the Win32 implementation is
    maybe bugged.
    
    The VS 15 build just exists and the resulting sjasmplus.exe is used only
    to assemble examples, without further verification of results.

---
+++ ContinuousIntegration/winbuild_vs15_2017.bat
@@ -0,0 +1,24 @@
+rem Exploring the VM image and what is available
+path
+
+rem CMAKE windows build with MS compiler
+del Makefile
+ren Makefile.win Makefile
+mkdir build
+cd build
+@rem cmake --help
+cmake --config Release ..
+dir /W
+@rem C:\tools\msys64\usr\bin\find "C:/Program Files (x86)/Microsoft Visual Studio" -iname vcvars64.bat -type f
+@rem C:\tools\msys64\usr\bin\find "C:/Program Files (x86)/Microsoft Visual Studio" -iname msbuild.exe -type f
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+path
+echo "Starting build by running msbuild.exe"
+msbuild sjasmplus.vcxproj /property:Configuration=Release
+echo "installing to c:\tools\sjasmplus"
+@echo on
+mkdir c:\tools\sjasmplus
+copy Debug\sjasmplus.exe c:\tools\sjasmplus\sjasmplus.exe
+copy Release\sjasmplus.exe c:\tools\sjasmplus\sjasmplus.exe
+dir /N c:\tools\sjasmplus\sjasmplus.exe
+c:\tools\sjasmplus\sjasmplus.exe --version
