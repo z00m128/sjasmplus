76a5ace297cbe9eb5e5a669e5c49185e0d80a463
Peter Helcmanovsky (Ped) <ped@7gods.org>
2019-08-29 02:39:38 +0200

    tests: renaming + refreshing "docs_examples/st_usage_example_6_5.asm"

--- tests/docs_examples/st_usage_example.asm
+++ tests/docs_examples/st_usage_example.asm
@@ -6,7 +6,7 @@ value2      WORD
         ORG  0x8000
 binData BIN_FILE_MAP = $        ; set up label values only (no bytes)
         ; INCBIN "some_data.bin"  ; load the bytes from file instead
-        INCBIN "st_usage_example_6_5.asm"   ; include the source itself to not error out
+        INCBIN "st_usage_example.asm"   ; include the source itself to not error out
 
         ; using the data through struct definition
         ld  a,(binData.value1)
