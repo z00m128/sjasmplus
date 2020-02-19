6d50d14aa2a366fe06deedc6a504f1fd974d90e2
Peter Helcmanovsky (Ped) <ped@7gods.org>
2020-02-19 20:32:02 +0100

    tests/macro_examples/sj_library.asm: added alignHL macro

--- tests/macro_examples/sj_library.asm
+++ tests/macro_examples/sj_library.asm
@@ -1,3 +1,5 @@
+    OPT push reset --syntax=abfw
+
     MACRO negR16toR16 fromR16?, toR16?
         ; 6B 24T, uses A
         xor     a
@@ -28,3 +30,52 @@
         sbc hl,de
         ex  de,hl
     ENDM
+
+    MACRO alignHl alignValue?
+        ASSERT 2 <= (alignValue?)
+        ASSERT 0 == ((alignValue?) & ((alignValue?)-1)) ; make sure it's power of two
+        push    af
+        IF (alignValue?) == 256
+            xor     a
+            cp      l           ; Fc=1 when 0 < L
+            ld      l,a         ; L = 0
+            adc     a,h
+            ld      h,a
+                ; 20T 5B
+        ELSE : IF (alignValue?) == 2
+            inc     hl
+            res     0,l
+                ; 14T 3B
+        ELSE : IF (alignValue?) < 256
+            ld      a,(alignValue?)-1
+            IFNDEF SJ_LIBRARY_USE_Z80N
+                add     a,l
+                rra             ; preserve carry flag for increment of H
+                and     -((alignValue?)>>1)     ; clear bottom bits, Fc=0
+                rla             ; restore add-carry and fix position of L bits
+                ld      l,a
+                adc     a,h     ; A = L + H + add-carry
+                sub     l       ; A = H + add-carry (new H)
+                ld      h,a
+                    ; 42T 11B
+            ELSE
+                add     hl,a    ; add align-1 to HL
+                cpl             ; bits to keep in L (and clear bottom bits)
+                and     l
+                ld      l,a
+                    ; 27T 7B
+            ENDIF
+        ELSE : ASSERT 256 < (alignValue?)
+            xor     a
+            cp      l
+            ld      l,a
+            adc     a,h     ; if 0 < L, then ++H here in every case
+            add     a,((alignValue?)-1)>>8
+            and     -((alignValue?)>>8)
+            ld      h,a
+                ; 34T 9B
+        ENDIF : ENDIF : ENDIF
+        pop     af
+    ENDM
+
+    OPT pop
