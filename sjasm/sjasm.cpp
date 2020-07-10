76ac81e6e8aa045d553c6fad4c18a9144374e914
Peter Helcmanovsky (Ped) <ped@7gods.org>
2020-07-10 11:49:06 +0200

    LGTM: hide alert about unsafe function call
    The call to `std::localtime` inside `main` should never become MT and
    the content of shared buffer is immediately copied to own memory after
    the call, so this is "false positive" in current code.
    
    The proposed work-around is unfortunately "C2x", so disabling the alert
    seems least harmful at this moment.

--- sjasm/sjasm.cpp
+++ sjasm/sjasm.cpp
@@ -626,7 +626,7 @@ int main(int argc, char **argv) {
 	// setup __DATE__ and __TIME__ macros (setup them just once, not every pass!)
 	auto now = std::chrono::system_clock::now();
 	std::time_t now_c = std::chrono::system_clock::to_time_t(now);
-	std::tm now_tm = *std::localtime(&now_c);
+	std::tm now_tm = *std::localtime(&now_c);	// lgtm [cpp/potentially-dangerous-function]
 	char dateBuffer[32] = {}, timeBuffer[32] = {};
 	SPRINTF3(dateBuffer, 30, "\"%04d-%02d-%02d\"", now_tm.tm_year + 1900, now_tm.tm_mon + 1, now_tm.tm_mday);
 	SPRINTF3(timeBuffer, 30, "\"%02d:%02d:%02d\"", now_tm.tm_hour, now_tm.tm_min, now_tm.tm_sec);
