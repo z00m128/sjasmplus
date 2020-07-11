8992a32de9356735a13c5c0f342ac0334e6c784b
Peter Helcmanovsky (Ped) <ped@7gods.org>
2020-07-11 19:37:19 +0200

    test coverage: parser.cpp: hunting internal implementation coverage (second try)

--- tests/lua_examples/trivial/lua_coverage2.asm
+++ tests/lua_examples/trivial/lua_coverage2.asm
@@ -8,3 +8,12 @@
     LUA ALLPASS
         sj.add_byte(_c("2+3"))
     ENDLUA xyz  ; either sline or sline2 should be non-empty from substitution
+    ; the above trick didn't help, so one more try
+    LUA ALLPASS
+        _pc("nop qwe")
+        sj.add_byte(_c("3+4"))
+    ENDLUA
+    LUA ALLPASS
+        _pc("nop xyz")
+        sj.add_byte(_c("5+6"))
+    ENDLUA
