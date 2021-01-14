814f9ad9dd8ab28883991d86ead343fb8241e8c1
Peter Helcmanovsky (Ped) <ped@7gods.org>
2021-01-14 15:59:07 +0100

    be-host #133: hunting the working endian.h includes on each platform

--- sjasm/support.h
+++ sjasm/support.h
@@ -31,22 +31,7 @@
 constexpr char pathBadSlash = '\\';
 constexpr char pathGoodSlash = '/';
 
-#if defined(__OSX__)
-#	include <machine/endian.h>
-#else
-#	include <endian.h>
-#endif
-
-// #if defined(__linux__)
-// #	include <endian.h>
-// #elif defined(__FreeBSD__) || defined(__NetBSD__)
-// #include <sys/endian.h>
-// #elif defined(__OpenBSD__)
-// #include <sys/types.h>
-// #define be16toh(x) betoh16(x)
-// #define be32toh(x) betoh32(x)
-// #define be64toh(x) betoh64(x)
-// #endif
+#include "portable_endian.h"
 
 #if defined (_MSC_VER)
 
