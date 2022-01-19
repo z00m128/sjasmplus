6ab8307de505fed4567abcf0d0aafa384cb7d947
Peter Helcmanovsky (Ped) <ped@7gods.org>
2022-01-19 20:52:05 +0100

    test-coverage: support.cpp - make it pass also in windows OS

--- tests/misc/support_cpp/test_coverage.asm
+++ tests/misc/support_cpp/test_coverage.asm
@@ -6,7 +6,7 @@
     ; absolute path is ok-ish (still pain if you cooperate with several developers on same project, but your choice)
     INCLUDELUA "c:/absolute_win_drive_path.some.nonexisting.file"
 
-    ; check LuaShellExec routine
+    ; check LuaShellExec routine - can't capture output (echo is stdout and redirect to stderr differs per OS)
     LUA PASS3
-        sj.shellexec(">&2 echo 'ahoy in stderr'")
+        sj.shellexec("echo hello")
     ENDLUA
