b0472b33a992344ef55d1416f2f23091e754cac2
Peter Helcmanovsky (Ped) <ped@7gods.org>
2022-05-21 22:11:07 +0200

    Revert "lua5.4: (WIP) temporary patch tests to pass all (to be adjusted later)"
    This reverts commit 30acc2a009f8b298f9345c821ec08d26c041abba.

--- tests/lua_examples/trivial/lua_emit_warning_sup.asm
+++ tests/lua_examples/trivial/lua_emit_warning_sup.asm
@@ -23,43 +23,43 @@
 ;; test suppression at beginning of block
 
     lua pass1   ; luamc-ok - warning should be suppressed (even here at beginning of block)
-        sj.warning("but lua warning should work: 11")
+        sj.warning("but lua warning should work", 11)
         sj.add_byte(11)
     endlua
 
     lua pass2   ; luamc-ok - warning should be suppressed (even here at beginning of block)
-        sj.warning("but lua warning should work: 12")
+        sj.warning("but lua warning should work", 12)
         sj.add_byte(12)
     endlua
 
     lua pass3   ; luamc-ok - warning should be suppressed (even here at beginning of block)
-        sj.warning("but lua warning should work: 13")
+        sj.warning("but lua warning should work", 13)
         sj.add_byte(13)
     endlua
 
     lua         ; luamc-ok - warning should be suppressed (even here at beginning of block)
-        sj.warning("but lua warning should work: 14")
+        sj.warning("but lua warning should work", 14)
         sj.add_byte(14)
     endlua
 
 ;; test suppression at end of block
 
     lua pass1
-        sj.warning("but lua warning should work: 21")
+        sj.warning("but lua warning should work", 21)
         sj.add_byte(21)
     endlua      ; luamc-ok
 
     lua pass2
-        sj.warning("but lua warning should work: 22")
+        sj.warning("but lua warning should work", 22)
         sj.add_byte(22)
     endlua      ; luamc-ok
 
     lua pass3
-        sj.warning("but lua warning should work: 23")
+        sj.warning("but lua warning should work", 23)
         sj.add_byte(23)
     endlua      ; luamc-ok
 
     lua
-        sj.warning("but lua warning should work: 24")
+        sj.warning("but lua warning should work", 24)
         sj.add_byte(24)
     endlua      ; luamc-ok
