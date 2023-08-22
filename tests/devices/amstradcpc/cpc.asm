d9240786d6c079a32e6a749e4a41e76418f1fe23
Laurent Carlier <lordheavym@gmail.com>
2023-08-22 17:13:26 +0200

    tests: add amstradcpcplus and savecpr tests

--- tests/devices/amstradcpc/cpc.asm
+++ tests/devices/amstradcpc/cpc.asm
@@ -56,3 +56,72 @@
     ORG 0xFFFE
     DB  "CCDD"  ; "DD" goes beyond 0x10000 -> lost (error reported)
     ASSERT {0} == "77"          ; still page 7 there
+
+    ; swap to the plus
+    DEVICE AMSTRADCPCPLUS
+
+    SLOT 0
+    PAGE 0 : ASSERT {0} == 0 : ORG 0x0000 : DB "00"
+    PAGE 1 : ASSERT {0} == 0 : ORG 0x0000 : DB "11"
+    PAGE 2 : ASSERT {0} == 0 : ORG 0x0000 : DB "22"
+    PAGE 3 : ASSERT {0} == 0 : ORG 0x0000 : DB "33"
+    PAGE 4 : ASSERT {0} == 0 : ORG 0x0000 : DB "44"
+    PAGE 5 : ASSERT {0} == 0 : ORG 0x0000 : DB "55"
+    PAGE 6 : ASSERT {0} == 0 : ORG 0x0000 : DB "66"
+    PAGE 7 : ASSERT {0} == 0 : ORG 0x0000 : DB "77"
+    PAGE 8 : ASSERT {0} == 0 : ORG 0x0000 : DB "88"
+    PAGE 9 : ASSERT {0} == 0 : ORG 0x0000 : DB "99"
+    PAGE 10 : ASSERT {0} == 0 : ORG 0x0000 : DB "AA"
+    PAGE 11 : ASSERT {0} == 0 : ORG 0x0000 : DB "BB"
+    PAGE 12 : ASSERT {0} == 0 : ORG 0x0000 : DB "CC"
+    PAGE 13 : ASSERT {0} == 0 : ORG 0x0000 : DB "DD"
+    PAGE 14 : ASSERT {0} == 0 : ORG 0x0000 : DB "EE"
+    PAGE 15 : ASSERT {0} == 0 : ORG 0x0000 : DB "FF"
+    PAGE 16 : ASSERT {0} == 0 : ORG 0x0000 : DB "GG"
+    PAGE 17 : ASSERT {0} == 0 : ORG 0x0000 : DB "HH"
+    PAGE 18 : ASSERT {0} == 0 : ORG 0x0000 : DB "II"
+    PAGE 19 : ASSERT {0} == 0 : ORG 0x0000 : DB "JJ"
+    PAGE 20 : ASSERT {0} == 0 : ORG 0x0000 : DB "KK"
+    PAGE 21 : ASSERT {0} == 0 : ORG 0x0000 : DB "LL"
+    PAGE 22 : ASSERT {0} == 0 : ORG 0x0000 : DB "MM"
+    PAGE 23 : ASSERT {0} == 0 : ORG 0x0000 : DB "NN"
+    PAGE 24 : ASSERT {0} == 0 : ORG 0x0000 : DB "OO"
+    PAGE 25 : ASSERT {0} == 0 : ORG 0x0000 : DB "PP"
+    PAGE 26 : ASSERT {0} == 0 : ORG 0x0000 : DB "QQ"
+    PAGE 27 : ASSERT {0} == 0 : ORG 0x0000 : DB "RR"
+    PAGE 28 : ASSERT {0} == 0 : ORG 0x0000 : DB "SS"
+    PAGE 29 : ASSERT {0} == 0 : ORG 0x0000 : DB "TT"
+    PAGE 30 : ASSERT {0} == 0 : ORG 0x0000 : DB "UU"
+    PAGE 31 : ASSERT {0} == 0 : ORG 0x0000 : DB "VV"
+
+    PAGE 32      ; error - non-existing page (page 31 should be still visible in slot 0)
+    ASSERT {0} == "VV"
+
+    SLOT 1 : PAGE 4 : ASSERT {0x4000} == "44" : PAGE 5 : ASSERT {0x4000} == "55"
+    SLOT 2 : PAGE 6 : ASSERT {0x8000} == "66" : PAGE 7 : ASSERT {0x8000} == "77"
+    SLOT 3 : PAGE 8 : ASSERT {0xC000} == "88" : PAGE 9 : ASSERT {0xC000} == "99"
+    SLOT 1 : PAGE 10 : ASSERT {0x4000} == "AA" : PAGE 11 : ASSERT {0x4000} == "BB"
+    SLOT 2 : PAGE 12 : ASSERT {0x8000} == "CC" : PAGE 13 : ASSERT {0x8000} == "DD"
+    SLOT 3 : PAGE 14 : ASSERT {0xC000} == "EE" : PAGE 15 : ASSERT {0xC000} == "FF"
+    SLOT 1 : PAGE 16 : ASSERT {0x4000} == "GG" : PAGE 17 : ASSERT {0x4000} == "HH"
+    SLOT 2 : PAGE 18 : ASSERT {0x8000} == "II" : PAGE 19 : ASSERT {0x8000} == "JJ"
+    SLOT 1 : PAGE 20 : ASSERT {0x4000} == "KK" : PAGE 21 : ASSERT {0x4000} == "LL"
+    SLOT 2 : PAGE 22 : ASSERT {0x8000} == "MM" : PAGE 23 : ASSERT {0x8000} == "NN"
+    SLOT 3 : PAGE 24 : ASSERT {0xC000} == "OO" : PAGE 25 : ASSERT {0xC000} == "PP"
+    SLOT 1 : PAGE 26 : ASSERT {0x4000} == "QQ" : PAGE 27 : ASSERT {0x4000} == "RR"
+    SLOT 2 : PAGE 28 : ASSERT {0x8000} == "SS" : PAGE 29 : ASSERT {0x8000} == "TT"
+    SLOT 3 : PAGE 30 : ASSERT {0xC000} == "UU" : PAGE 31 : ASSERT {0xC000} == "VV"
+    SLOT 2 : PAGE 30 : ASSERT {0x8000} == "UU" : PAGE 31 : ASSERT {0x8000} == "VV"
+    SLOT 3 : PAGE 26 : ASSERT {0xC000} == "QQ" : PAGE 27 : ASSERT {0xC000} == "RR"
+
+    SLOT 4      ; error
+
+    ; pages: 31:27:31:27
+    ORG 0xC000-2
+    DB  "AABB"
+    ASSERT {0x4000-2} == "AA"   ; should be visible also at these addresses
+    ASSERT {0x4000} == "BB"
+
+    ORG 0xFFFE
+    DB  "CCDD"  ; "DD" goes beyond 0x10000 -> lost (error reported)
+    ASSERT {0} == "VV"          ; still page 31 there
---
+++ tests/devices/amstradcpc/savecprCoverage.asm
@@ -0,0 +1,13 @@
+; test-coverage cases not covered by regular tests
+
+    DEVICE AMSTRADCPC6128
+    SAVECPR "BadDevice.cpr", 2        ; error about wrong device
+
+    DEVICE NONE
+    SAVECPR "NoDevice.cpr", 1         ; error about none device
+
+    DEVICE AMSTRADCPCPLUS
+    SAVECPR "file.cpr", -1            ; negative number of pages
+    SAVECPR "file.cpr", &             ; invalid (parse) page value
+    SAVECPR "file.cpr", 33            ; page value out of bound
+    SAVECPR ".", 19                   ; fail to open file for write
