0a6a04a7d50313e36e6256240ce831aadb8c71cf
pleumann <joerg.pleumann@gmail.com>
2025-09-14 18:05:29 +0200

    Added test case for Fuse breakpoints

---
+++ tests/misc/breakpoints/dir_setbp_fuse.asm
@@ -0,0 +1,24 @@
+    device noslot64k
+
+    org $8000
+
+    setbp
+    nop
+    setbp $
+    nop
+    setbp $9000
+    nop
+
+    setbp "z80:bc!=0"
+    nop
+    setbp $,"z80:de!=0"
+    nop
+    setbp $A000,"z80:hl=0"
+    nop
+
+    setbp &
+    nop
+    setbp "on monday" !WTF?
+    nop
+
+    bplist "dir_setbp_fuse.exp" fuse
